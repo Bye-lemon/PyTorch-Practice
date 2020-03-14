aabf2729470fb50a8da27dfba2ce981aab08f5ea
Li-Air <bye_lemon@mail.dlut.edu.cn>
2020-03-14 15:24:33 +0800

    feat: Added Impl. of network GoogLeNet with Inception V1

--- models/CNN.py
+++ models/CNN.py
@@ -108,3 +108,124 @@ class VGG19(nn.Module):
         x = self.classifier(x)
         x = F.softmax(x, dim=1000)
         return x
+
+
+class _BasicConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super(_BasicConv2d, self).__init__()
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, **kwargs)
+        self.bn = nn.BatchNorm2d(num_features=out_channels)
+
+    def forward(self, x):
+        x = F.relu(self.bn(self.conv(x)))
+        return x
+
+
+class _InceptionV1(nn.Module):
+    def __init__(self, in_channels, out_ch1x1, out_ch3x3re, out_ch3x3, out_ch5x5re,
+                 out_ch5x5, pool_proj):
+        super(_InceptionV1, self).__init__()
+        self.branch1x1 = _BasicConv2d(in_channels=in_channels, out_channels=out_ch1x1, kernel_size=1)
+        self.branch3x3 = nn.Sequential(
+            _BasicConv2d(in_channels=in_channels, out_channels=out_ch3x3re, kernel_size=1),
+            _BasicConv2d(in_channels=out_ch3x3re, out_channels=out_ch3x3, kernel_size=3, padding=1)
+        )
+        self.branch5x5 = nn.Sequential(
+            _BasicConv2d(in_channels=in_channels, out_channels=out_ch5x5re, kernel_size=1),
+            _BasicConv2d(in_channels=out_ch5x5re, out_channels=out_ch5x5, kernel_size=5, padding=2)
+        )
+        self.branch_pool = nn.Sequential(
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+            _BasicConv2d(in_channels=in_channels, out_channels=pool_proj, kernel_size=1)
+        )
+
+    def forward(self, x):
+        out1 = self.branch1x1(x)
+        out2 = self.branch3x3(x)
+        out3 = self.branch5x5(x)
+        out4 = self.branch_pool(x)
+        x = torch.cat([out1, out2, out3, out4], dim=1)
+        return x
+
+
+class _Inception_Auxiliary(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super(_Inception_Auxiliary, self).__init__()
+        self.conv = nn.Sequential(
+            nn.AvgPool2d(kernel_size=5, stride=3),
+            _BasicConv2d(in_channels=in_channels, out_channels=128, kernel_size=1),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=2048, out_features=1024),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(in_features=1024, out_features=num_classes),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.view(-1, 2048)
+        x = self.classifier(x)
+        return F.softmax(x)
+
+
+class GoogLeNet(nn.Module):
+    def __init__(self):
+        super(GoogLeNet, self).__init__()
+        self.step1 = nn.Sequential(
+            _BasicConv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+            _BasicConv2d(in_channels=64, out_channels=64, kernel_size=1),
+            _BasicConv2d(in_channels=64, out_channels=192, kernel_size=3, padding=1),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True)
+        )
+        self.inception3 = nn.Sequential(
+            _InceptionV1(in_channels=192, out_ch1x1=64, out_ch3x3re=96, out_ch3x3=128,
+                         out_ch5x5re=16, out_ch5x5=32, pool_proj=32),
+            _InceptionV1(in_channels=256, out_ch1x1=128, out_ch3x3re=128, out_ch3x3=192,
+                         out_ch5x5re=32, out_ch5x5=96, pool_proj=64),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True)
+        )
+        self.inception4a = _InceptionV1(in_channels=480, out_ch1x1=192, out_ch3x3re=96, out_ch3x3=208,
+                                        out_ch5x5re=16, out_ch5x5=48, pool_proj=64)
+        self.aux1 = _Inception_Auxiliary(in_channels=480, num_classes=1000)
+        self.inception4b = _InceptionV1(in_channels=512, out_ch1x1=160, out_ch3x3re=112, out_ch3x3=224,
+                                        out_ch5x5re=24, out_ch5x5=64, pool_proj=64)
+        self.inception4c = _InceptionV1(in_channels=512, out_ch1x1=128, out_ch3x3re=128, out_ch3x3=256,
+                                        out_ch5x5re=24, out_ch5x5=64, pool_proj=64)
+        self.inception4d = _InceptionV1(in_channels=512, out_ch1x1=112, out_ch3x3re=144, out_ch3x3=288,
+                                        out_ch5x5re=32, out_ch5x5=64, pool_proj=64)
+        self.aux2 = _Inception_Auxiliary(in_channels=528, num_classes=1000)
+        self.inception4e = _InceptionV1(in_channels=528, out_ch1x1=256, out_ch3x3re=160, out_ch3x3=320,
+                                        out_ch5x5re=32, out_ch5x5=128, pool_proj=128)
+        self.max_pool = nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True)
+        self.inception5 = nn.Sequential(
+            _InceptionV1(in_channels=832, out_ch1x1=256, out_ch3x3re=160, out_ch3x3=320,
+                         out_ch5x5re=32, out_ch5x5=128, pool_proj=128),
+            _InceptionV1(in_channels=832, out_ch1x1=384, out_ch3x3re=192, out_ch3x3=384,
+                         out_ch5x5re=48, out_ch5x5=128, pool_proj=128),
+            nn.AvgPool2d(kernel_size=7, ceil_mode=True)
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.4),
+            nn.Linear(in_features=1024, out_features=1000)
+        )
+
+    def forward(self, x):
+        x = self.step1(x)
+        x = self.inception3(x)
+        aux1 = self.aux1(x)
+        x = self.inception4a(x)
+        x = self.inception4b(x)
+        x = self.inception4c(x)
+        x = self.inception4d(x)
+        aux2 = self.aux2(x)
+        x = self.inception4e(x)
+        x = self.max_pool(x)
+        x = self.inception5(x)
+        x = x.view(-1, 1024)
+        x = self.classifier(x)
+        x = F.softmax(x)
+        return aux1, aux2, x
+
