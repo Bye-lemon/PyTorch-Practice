89316d4582b87ef0c05b2985d5ac0b7a3273c2e6
Li-Air <Bye_lemon@mail.dlut.edu.cn>
2020-04-03 10:22:04 +0800

    revert: Remove unused file

--- main.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torchvision
-
-from models.CNN import AlexNet
-
-my_alex = AlexNet()
-alex = torchvision.models.alexnet(pretrained=True)
-my_alex.fc3.weight = alex.classifier[6].weight
-print(my_alex.fc3.weight)
